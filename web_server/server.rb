370460becd6e052a6e49a485691e409cc15feda8
Alex Weisberger <alex.m.weisberger@gmail.com>
2020-08-30 19:32:24 -0400

    Create people with form

--- web_server/server.rb
+++ web_server/server.rb
@@ -1,12 +1,22 @@
 require 'sinatra'
 require_relative '../personal_finance'
 
+model = PersonalFinance::Model.new
+
+all_people = [
+  model.create_person('Person 1'),
+  model.create_person('Person 2'),
+  model.create_person('Person 3'),
+  model.create_person('Person 4'),
+]
+
 get '/' do
-  people = [
-    PersonalFinance::Model.new.create_person('Person 1'),
-    PersonalFinance::Model.new.create_person('Person 2'),
-    PersonalFinance::Model.new.create_person('Person 3'),
-    PersonalFinance::Model.new.create_person('Person 4'),
-  ]
-  erb :people, locals: { people: people }
+  @people = all_people
+  erb :home
+end
+
+post '/people' do
+  all_people << model.create_person(params[:name])
+  @people = all_people
+  erb :home
 end
