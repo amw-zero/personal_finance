9d3710f69cc7b2dd1ce7c2daf0b465a1939827af
Alex Weisberger <alex.m.weisberger@gmail.com>
2020-09-04 14:25:49 -0400

    Change test to check for ordering of transaction dates

--- spec/view_cash_flow_spec.rb
+++ spec/view_cash_flow_spec.rb
@@ -1,6 +1,5 @@
 require_relative 'test_application'
 
-
 describe 'Viewing cash flow' do
   subject { test_application }
 
@@ -8,12 +7,12 @@ describe 'Viewing cash flow' do
   let(:savings_account) { subject.create_account('Savings') }
 
   before do
-    subject.create_transaction(account_id: checking_account.id, amount: 100.0, currency: :usd, day_of_month: 1)
+    subject.create_transaction(account_id: checking_account.id, amount: 100.0, currency: :usd, day_of_month: 2)
     subject.create_transaction(account_id: checking_account.id, amount: 200.0, currency: :usd, day_of_month: 1)
     subject.create_transaction(account_id: savings_account.id, amount: 200.0, currency: :usd, day_of_month: 1)
   end
 
   it do
-    expect(subject.cash_flow(checking_account.id).map(&:amount)).to eq([100, 200])
+    expect(subject.cash_flow(checking_account.id).map(&:amount)).to eq([200, 100])
   end
 end
