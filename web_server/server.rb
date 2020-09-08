1710969f37bdcc69f2843f26f9fc433c3de0c58f
Alex Weisberger <alex.m.weisberger@gmail.com>
2020-09-08 00:06:44 -0400

    tagged transaction grid

--- web_server/server.rb
+++ web_server/server.rb
@@ -68,4 +68,8 @@ helpers do
   def input_classes
     'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
   end
+
+  def label_classes
+    'block text-gray-700 text-sm font-bold mb-2'
+  end
 end
