616f3a5a8eb6a92fb639536a1d8eaf9540512f4d
Alex Weisberger <alex.m.weisberger@gmail.com>
2021-02-11 22:41:49 -0500

    Cascade deletes so you can delete connected data

---
+++ postgres/05_cascade_transaction_deletes.rb
@@ -0,0 +1,14 @@
+require 'sequel'
+require_relative 'postgres'
+
+DB = Sequel.connect(Postgres::SERVER_URL)
+
+DB.alter_table(:transactions) do
+  drop_foreign_key :account_id
+  add_foreign_key :account_id, :accounts, on_delete: :cascade
+end
+
+DB.alter_table(:transaction_tags) do
+  drop_foreign_key :transaction_id
+  add_foreign_key :transaction_id, :transactions, on_delete: :cascade
+end
