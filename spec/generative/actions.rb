cfd18507b21c3deafc46269ae96a0a6923f48946
Alex Weisberger <alex.m.weisberger@gmail.com>
2021-02-12 22:35:02 -0500

    Reduce test output.
    The result of built_as counts as a single given value, instead of counting every intermediate value sampled when creating these values.

--- spec/generative/actions.rb
+++ spec/generative/actions.rb
@@ -16,25 +16,31 @@ module ApplicationActions
 
     case action
     when :create_account
-      account_name = any strings, name: 'Account Name'
-      test_app.create_account(account_name)
+      any built_as do
+        account_name = any strings, name: 'Account Name'
+        test_app.create_account(account_name)
+      end
     when :create_transaction
       return if test_app.accounts.empty?
 
-      account = any element_of(test_app.accounts), name: 'Transaction Account'
-      amount = any integers(min: 1, max: 500), name: 'Transaction Amount'
-      test_app.create_transaction(
-        name: any(strings),
-        account_id: account.id,
-        amount: amount.to_f,
-        currency: :usd,
-        day_of_month: any(integers(min: 1, max: 31))
-      )
+      any built_as do
+        account = any element_of(test_app.accounts), name: 'Transaction Account'
+        amount = any integers(min: 1, max: 500), name: 'Transaction Amount'
+        test_app.create_transaction(
+          name: any(strings),
+          account_id: account.id,
+          amount: amount.to_f,
+          currency: :usd,
+          day_of_month: any(integers(min: 1, max: 31))
+        )
+      end
     when :create_tag
       return if test_app.all_transactions.transactions.empty?
 
-      transaction = any(element_of(test_app.all_transactions.transactions))
-      test_app.tag_transaction(transaction.id, tag: any(strings))
+      any built_as do
+        transaction = any(element_of(test_app.all_transactions.transactions))
+        test_app.tag_transaction(transaction.id, tag: any(strings))
+      end
     when :create_tag_set
       # params = {
       #   transaction_tag: [String]
@@ -42,20 +48,22 @@ module ApplicationActions
       # use_case = [String] -> [TransactionTag] -> ApplicationState
       # Would be good to have a domain constraint, i.e. "Strings are valid TransactionTags"
 
-      tags = test_app.transaction_tags.map(&:name)
-      return if tags.empty?
+      any built_as do
+        tags = test_app.transaction_tags.map(&:name)
+        return if tags.empty?
 
-      bad_inputs = %w[jkl randM]
-      tag_inputs = from(element_of(tags), element_of(bad_inputs))
+        bad_inputs = %w[jkl randM]
+        tag_inputs = from(element_of(tags), element_of(bad_inputs))
 
-      params = any(
-        hashes_of_shape(
-          title: strings,
-          transaction_tag: arrays(of: tag_inputs)
+        params = any(
+          hashes_of_shape(
+            title: strings,
+            transaction_tag: arrays(of: tag_inputs)
+          )
         )
-      )
 
-      test_app.create_transaction_tag_set(params)
+        test_app.create_transaction_tag_set(params)
+      end
     else
       raise "Attempted to execute unknown action: #{action}"
     end
